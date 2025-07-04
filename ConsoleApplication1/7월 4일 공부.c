c1489b2dee23e1e4326b01d7c41233967e597336
jongsoeng <user@ENG8-815-ST8>
2025-07-04 12:31:28 +0900

    7월 4일 복습할 내용

--- ConsoleApplication1/7월 4일 공부.c
+++ ConsoleApplication1/7월 4일 공부.c
@@ -1,6 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
 #include<time.h>
 //void uf1(); //함수 원형 //함수 선언
 //int udf2() {
@@ -21,23 +24,46 @@
 //	return factorial;
 //}
 // call by value 방식
-void swap(int x, int y) { // a=10 b=20
+//void swap(int x,int y) { // a=10 b=20
+//	int tmp;
+//	printf("swap 함수 내:");
+//	printf("x=%d y=%d\n", x, y);
+//	tmp = x;
+//	x = y;
+//	y = tmp;
+//	printf("swap 함수 내 교환 후:");
+//	printf("x=%d y=%d\n", x, y);
+//}
+void swap(int* x, int* y) { // & *
 	int tmp;
 	printf("swap 함수 내:");
-	printf("x=%d y=%d\n", x, y);
-	tmp = x;
-	x = y;
-	y = tmp;
+	printf("x=%d y=%d\n", *x, *y);
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+
 	printf("swap 함수 내 교환 후:");
-	printf("x=%d y=%d\n", x, y);
+	printf("x=%d y=%d\n", *x, *y);
 }
 void main() {
 	int x = 10; int y = 20;
 	printf("교환 전: ");
 	printf("x=%d y=%d\n", x, y);
-	swap(x, y);
+	swap(&x, &y);
 	printf("교환 후: ");
 	printf("x=%d y=%d\n", x, y);
+	//long long  x =10;
+	//long long *xp; //포인터가 총 8byte로 메모리에 저장
+	//xp = &x; //xp라는 포인터에 x의 주소를 입력
+	//printf("x =%ld,주소 =%p,크기=%d \n", x, &x, sizeof(x));
+	//printf("xp = %p xp 크기= %d  xp주소 =%p \n", xp,sizeof(xp),&xp);
+
+	/*int x = 10; int y = 20;
+	printf("교환 전: ");
+	printf("x=%d y=%d\n", x, y);
+	swap(x, y);
+	printf("교환 후: ");
+	printf("x=%d y=%d\n", x, y);*/
 	/*int n;*/
 	//int total = 0; //반드시 0으로 초기화해야 누적 합 계산이 올바르게 됨. 초기화하지 않으면 쓰레기 값이 들어가 결과가 이상해짐.
 	//int factorial = 1; //팩토리얼 이므로 0!=1로 초기화 해야 올바르게 !계산이 됨. 이유는 누적 합과 동일함
