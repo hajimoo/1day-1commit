f1825d1118fa766a55ac23fd373e2c7b87292849
hajimoo <itrust67709901@gmail.com>
2025-07-08 19:19:34 +0900

    7월 8일 복습

--- ConsoleApplication1/ConsoleApplication1/7월 8일 공부.c
+++ ConsoleApplication1/ConsoleApplication1/7월 8일 공부.c
@@ -1,9 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
+ï»¿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void main()
 {
-	/*typedef struct student {
+	//typedef struct student STUDENT;
+	//typedef long long lint; // long long ëŒ€ì‹  lintë¡œ ì“´ë‹¤ëŠ” ê±° //lint a;
+	typedef struct student {
 		int hakbun;
 		char nam[20];
 		int mid;
@@ -12,78 +14,66 @@ void main()
 		int total;
 		float avg;
 	}STUDENT;
-	STUDENT stu= { 1001,"mkchoi",30,28,28,0,0.0 };
-	STUDENT* p;
-	p = &stu;
-	printf("%3d %s %3d %3d %3d\n", (*p).hakbun, (*p).nam, (*p).mid, (*p).final, (*p).quize);
-	printf("%3d %s %3d %3d %3d\n", p->hakbun, p->nam, p->mid, p->final, p->quize);*/
-	// À§Ä¡(À§µµ,°æµµ) Á¤º¸¸¦ Ç¥ÇöÇÏ´Â ±¸Á¶Ã¼(position) Á¤ÀÇ
-   //struct position{
-   //double  latitude;
-   //double longitude;
-   //};
-   // µµ½Ã ÀÌ¸§,À§Ä¡¸¦ Ç¥ÇöÇÏ´Â ±¸Á¶Ã¼(city)
-   //struct city {
-   //	//char *name; // char Æ÷ÀÎÅÍ
-   //	char name[20]; // char ¹è¿­
-   //	struct position place;
-   //};
-   //struct city  seoul;
-   ///*seoul.name = "seoul";*/ //Æ÷ÀÎÅÍ
-   //strcpy(seoul.name, "seoul");   // ¹è¿­
-   //seoul.place.latitude = 37.33;
-   //seoul.place.longitude = 126.58;
-   //struct city newyork = { "newyork",{40.8,73.9} };
-   //printf("%s %f %f", seoul.name, seoul.place.latitude, seoul.place.longitude);  
 
-	typedef struct fraction {
-		int mumerator;
-		int denominator;
-	}ºĞ¼ö;
-	ºĞ¼ö stu1 = { 2,3 };
-	ºĞ¼ö stu2 = { 4,5 };
-
-	printf("%d / %d  * %d /%d = %d / %d\n", stu1.mumerator, stu1.denominator, stu2.mumerator, stu2.denominator, (stu1.mumerator * stu2.mumerator), (stu1.denominator * stu2.denominator));
-	printf("%d / %d / %d / %d = %d / %d\n", stu1.mumerator, stu1.denominator, stu2.mumerator, stu2.denominator, (stu1.mumerator * stu2.denominator), (stu2.mumerator * stu1.denominator));
-	//typedef struct student STUDENT;
-	//typedef long long lint; // long long ´ë½Å lint·Î ¾´´Ù´Â °Å
-
-	//lint a;
-
-	//typedef struct STUDENT;
-	//STUDENT stu1 = { "mkchoi",1001,30,30,20,0,0.0 };
-	/*STUDENT stu2;
-	stu2.hakbun = 1002;
-	stu2.name = "jkim";
-	stu2.mid = 30; */
-	//STUDENT stu[3] = { {1001,"mkchoi",30,28,28,0,0.0},
-	//	{1002,"jskim",30,50,28,0,0.0},
+	//STUDENT stu[3] = {{1001,"mkchoi",30,28,28,0,0.0},
+	//		{1002,"jskim",30,50,28,0,0.0},
 	//	{1003,"pklee",25,25,25,0,0.0} };
-	/*printf("ÇĞ¹ø\t ÀÌ¸§\t Áß°£\t±â¸»\tÄûÁî Á¡¼ö ÀÔ·Â: \n");
+	//printf("í•™ë²ˆ\t ì´ë¦„\t ì¤‘ê°„\tê¸°ë§\tí€´ì¦ˆ\t ì´ì \tí‰ê· \n");
+	///*printf("%d\t %s\t %d\t %d \t %d\n ", stu[0].hakbun, stu[0].nam, stu[0].mid, stu[0].final, stu[0].quize);
+	//printf("%d\t %s\t %d\t %d \t %d\n ", stu[1].hakbun, stu[1].nam, stu[1].mid, stu[1].final, stu[1].quize);
+	//printf("%d\t %s\t %d\t %d \t %d\n ", stu[2].hakbun, stu[2].nam, stu[2].mid, stu[2].final, stu[2].quize);*/ // ë°˜ë³µë¬¸
+	//for (int i = 0; i <= 2; i++) {
+	//	stu[i].total = stu[i].mid + stu[i].final + stu[i].quize;
+	//	stu[i].avg = stu[i].total / 3.0;
+	//	printf("%d\t %s\t %d\t %d \t %d\t ", stu[i].hakbun, stu[i].nam, stu[i].mid, stu[i].final, stu[i].quize);
+	//	printf("%3d\t %.2f\n", stu[i].total, stu[i].avg);
+	//}
+	//typedef struct fraction {
+	//	int mumerator;
+	//	int denominator;
+	//}ë¶„ìˆ˜;
+	//ë¶„ìˆ˜ stu1 = { 2,3 };
+	//ë¶„ìˆ˜ stu2 = { 4,5 };
+
+	//printf("%d / %d  * %d /%d = %d / %d\n", stu1.mumerator, stu1.denominator, stu2.mumerator, stu2.denominator, (stu1.mumerator* stu2.mumerator), (stu1.denominator* stu2.denominator));
+	//printf("%d / %d / %d / %d = %d / %d\n", stu1.mumerator, stu1.denominator, stu2.mumerator, stu2.denominator, (stu1.mumerator* stu2.denominator), (stu2.mumerator* stu1.denominator));
+ //  
+	//// ìœ„ì¹˜(ìœ„ë„,ê²½ë„) ì •ë³´ë¥¼ í‘œí˜„í•˜ëŠ” êµ¬ì¡°ì²´(position) ì •ì˜
+ //  struct position{
+ //  double  latitude;
+ //  double longitude;
+ //  };
+ //  // ë„ì‹œ ì´ë¦„,ìœ„ì¹˜ë¥¼ í‘œí˜„í•˜ëŠ” êµ¬ì¡°ì²´(city)
+ //  struct city {
+ //  char *name; // char í¬ì¸í„°
+ //  //char name[20]; // char ë°°ì—´
+ //  struct position place;
+ //  };
+ //  struct city  seoul;
+ //  seoul.name = "seoul"; //í¬ì¸í„°
+ //  //strcpy(seoul.name, "seoul");   // ë°°ì—´
+ //  seoul.place.latitude = 37.33;
+ //  seoul.place.longitude = 126.58;
+ //  struct city newyork = { "newyork",{40.8,73.9} };
+ //  printf("%s %f %f\n", seoul.name, seoul.place.latitude, seoul.place.longitude);  
+ //  printf("%s %f %f\n", newyork.name, newyork.place.latitude, newyork.place.longitude);
+
+
+   /*printf("í•™ë²ˆ\t ì´ë¦„\t ì¤‘ê°„\tê¸°ë§\tí€´ì¦ˆ ì ìˆ˜ ì…ë ¥: \n");
+   STUDENT stu[3];
 	for (int i = 0; i <= 2; i++) {
 		scanf("%d%s%d%d %d", &stu[i].hakbun, stu[i].nam, &stu[i].mid, &stu[i].final, &stu[i].quize);
 	}
-	printf("ÇĞ¹ø\t ÀÌ¸§\t Áß°£\t±â¸»\tÄûÁî\t ÃÑÁ¡\tÆò±Õ\n");
-	for (int i=0; i<=2;i++){
+	printf("í•™ë²ˆ\t ì´ë¦„\t ì¤‘ê°„\tê¸°ë§\tí€´ì¦ˆ\t ì´ì \tí‰ê· \n");
+	for (int i = 0; i <= 2; i++) {
 		stu[i].total = stu[i].mid + stu[i].final + stu[i].quize;
 		stu[i].avg = stu[i].total / 3.0;
 		printf("%d\t %s\t %d\t %d \t %d\t ", stu[i].hakbun, stu[i].nam, stu[i].mid, stu[i].final, stu[i].quize);
-		printf("%3d\t %2.f\n", stu[i].total, stu[i].avg);
+		printf("%3d\t %.2f\n", stu[i].total, stu[i].avg);
 	}*/
-	/*printf("%d\t %s\t %d\t %d \t %d\n ", stu[0].hakbun, stu[0].nam, stu[0].mid, stu[0].final, stu[0].quize);
-	printf("%d\t %s\t %d\t %d \t %d\n ", stu[1].hakbun, stu[1].nam, stu[1].mid, stu[1].final, stu[1].quize);
-	printf("%d\t %s\t %d\t %d \t %d ", stu[2].hakbun, stu[2].nam, stu[2].mid, stu[2].final, stu[2].quize);*/
-	//printf("%3d\t %2.f\n", stu1.total, stu1.avg);
-	/*printf("%d %d", sizeof(STUDENT), sizeof(stu[0]));
-	printf("%d", sizeof(stu));*/
-	//STUDENT stu1;//= { "mkchoi",1001,30,30,20,0,0.0 };
-	//printf("ÇĞ¹ø\t ÀÌ¸§\t Áß°£\t±â¸»\tÄûÁî Á¡¼ö ÀÔ·Â: \n");
-	//scanf("%d%s%d%d %d", &stu1.hakbun, stu1.nam, &stu1.mid, &stu1.final, &stu1.quize);
-	//scanf("%d%s%d%d %d", &stu2.hakbun, stu2.nam, &stu2.mid, &stu2.final, &stu2.quize);
-	//stu1.total =  stu1.mid + stu1.final+stu1.quize;
-	//stu1.avg = stu1.total / 3.0;
-	/*printf("ÇĞ¹ø\t ÀÌ¸§\t Áß°£\t±â¸»\tÄûÁî\t ÃÑÁ¡\tÆò±Õ\n");
-	printf("%d\t %s\t %d\t %d \t %d ", stu1.hakbun,stu1.nam,stu1.mid,stu1.final,stu1.quize);
-	printf("%3d\t %2.f\n", stu1.total, stu1.avg);*/
-	//struct student stu2;
+
+	/*STUDENT stu = { 1001, "mkchoi", 30, 28, 28, 0, 0.0 };
+	STUDENT* p = &stu;
+	printf("%3d %s %3d %3d %3d\n", (*p).hakbun, (*p).nam, (*p).mid, (*p).final, (*p).quize);
+	printf("%3d %s %3d %3d %3d\n", p->hakbun, p->nam, p->mid, p->final, p->quize);*/
 }
