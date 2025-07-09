92393d5ac4939e294fb35bd0d1c1ad74de5a5122
hajimoo <itrust67709901@gmail.com>
2025-07-10 01:04:44 +0900

    .

--- ConsoleApplication1/ConsoleApplication1/7월 9일 공부.c
+++ ConsoleApplication1/ConsoleApplication1/7월 9일 공부.c
@@ -95,4 +95,81 @@ void main() {
 
 		printf("%d / %d", tp->numer, tp->demon);
 		*/
+
+	/*typedef struct human {
+		int num;
+		char name[20];
+		human* next;
+	}human;
+	human* head;
+	head = (human*)malloc(sizeof(human));*/ 링크드리스트를 위한 동적할당과 구조체 기본설명
+	/*int* test;
+
+	test = (int*)malloc(sizeof(int));
+
+	test[0] = 5;
+	printf("[0]%d\n", test[0]);
+
+	test = (int*)realloc(test, sizeof(int) * 2);
+
+	test[1] = 10;
+	printf("[0]%d\n", test[0]);
+	printf("[1]%d\n", test[1]);*/
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//	typedef struct data {
+//		int num;
+//		char name[20];
+//		struct data* next;
+//	}data;
+//	data* g_head = NULL;
+//	data* g_tail = NULL;
+//	char name[20];
+//	void printAll() {
+//		data* temp = g_head;
+//		while (temp->next) {
+//			printf("=s=\n");
+//			printf("num:%d\n", temp->num);
+//			printf("name:%s\n", temp->name);
+//			printf("=e=\n");
+//			temp = temp->next;
+//		}
+//		printf("=s=\n");
+//		printf("num:%d\n", temp->num);
+//		printf("name:%s\n", temp->name);
+//		printf("=e=\n");
+//	}
+//	int insert(int num, char* name)
+//	{
+//		data* node = malloc(sizeof(data));
+//		node->num = num;
+//		if (name != NULL) {
+//			strcpy(node->name, name);
+//		}
+//		node->next = NULL;
+//		if (g_head == NULL) {
+//			g_head = node;
+//			g_tail = node;
+//			return 1;
+//		}
+//		else {
+//			data* temp = g_head;
+//			while (temp->next) {
+//				temp = temp->next;
+//			}
+//			temp->next = node;
+//			g_tail = node;
+//			return 1;
+//		}
+//		return 0;
+//	}
+//	int main() {
+//		for (int i = 0; i < 10; i++) {
+//			sprintf(name, "test %d", i);
+//			insert(i, name);
+//		}
+//		printAll();
+//		return 1; // 링크드 리스트 정리
+//	}
 }
